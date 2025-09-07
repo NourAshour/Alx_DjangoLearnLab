82bf77fe1d30605c1c226051ffb58bc6fae18e82
NourAshour <nashour767@gmail.com>
2025-09-07 18:40:49 +0300

    Model

--- Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bookshelf.apps.BookshelfConfig'
 ]
 
 MIDDLEWARE = [
@@ -76,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'Nour',
+        'PASSWORD': 'Nour123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
