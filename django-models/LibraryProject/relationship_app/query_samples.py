5118c0bb0953dd42bddfb9f25e047c6eeebcd1a3
NourAshour <nashour767@gmail.com>
2025-09-08 17:58:23 +0300

    Edit

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,7 +1,7 @@
 from models import Book, Library, Librarian
 books = Book.objects.filter(author = 'author1')
 
-books = Library.objects.get(name='library_name')
+books = Library.objects.get(name=library_name)
 books.all()
 
 lib1_librarian = Librarian.objects.filter(library = 'lib1')
