cb76ee456bb6e715c09797db420db63358321738
NourAshour <nashour767@gmail.com>
2025-09-13 16:28:15 +0300

    .

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,8 +1,8 @@
 from models import Book, Author, Library, Librarian
-author = Author.objects.get(name=author_name)
+author = Author.objects.get(name='author_name')
 books = Book.objects.filter(author=author)
 
-books = Library.objects.get(name=library_name)
+library = Library.objects.get(name='library_name')
 books.all()
 
 lib1_librarian = Librarian.objects.get(library='lib1')
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import DetailView
+from relationship_app.models import Book, Library
 
-# Create your views here.
+def BookView(request):
+    books = Book.objects.all()
+    context = {'books' : books}
+    return render(request, 'booklist.html')
+
+class LibraryDetail(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        library = self.get_object()  
+
